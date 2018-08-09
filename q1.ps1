70c75ea7cb30465fbce204e687ad61dfd4162aa7
chuwerks <31015483+chuwerks@users.noreply.github.com>
2018-08-09 00:25:12 -0700

    answer to question 1
    3 and 5

---
+++ q1.ps1
@@ -0,0 +1,21 @@
+﻿
+$count = 0
+$a = 3,5
+$maxnum=1000
+foreach($i in $a){
+    $x=1
+    $intsub=0
+    Do
+    {
+        $intsub= $i * $x
+ 
+        if($intsub -lt $maxnum){
+           $count=$count + $intsub
+        }
+      $x++  
+     }
+     while($x -lt $maxnum)
+     #Write-Host $count -ForegroundColor DarkGray
+     $intsub =0 
+}
+write-host "Total "$count -ForegroundColor Cyan
